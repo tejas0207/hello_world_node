0a88395974d81ceadfb5c5cd3ebc94d405180d8e
Tejas Gupta <67390503+tejas0207@users.noreply.github.com>
2023-05-30 16:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world! ok done');
+    res.send('Pipeline Done');
 });
 
 app.listen(3000, () => {
