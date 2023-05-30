5c55706ddd4568327568b5dc7e22efe128fd7bd0
Tejas Gupta <67390503+tejas0207@users.noreply.github.com>
2023-05-30 12:06:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world Tejas 2!');
+    res.send('Hello, world Tejas please!');
 });
 
 app.listen(3000, () => {
